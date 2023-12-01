73d1f593da17d5e267afd73b89226ccbd95ba029
wangzhizhou <wangzhizhou@bytedance.com>
2023-12-01 15:25:11 +0800

    完成文本、图片、富文本、群分享消息发送

--- Sources/OrzLarkSDK/Message.swift
+++ Sources/OrzLarkSDK/Message.swift
@@ -14,7 +14,7 @@ final class Message: Codable {
         case text
         case post
         case image
-        case shareChat
+        case shareChat = "share_chat"
         case interactive
     }
     
@@ -22,16 +22,87 @@ final class Message: Codable {
     
     struct Content: Codable {
         
-        var text: String
+        var text: String?
+        
+        struct Post: Codable {
+            
+            struct Body: Codable {
+                
+                enum Tag: String, Codable {
+                    case text
+                    case a
+                    case at
+                    case img
+                }
+                
+                struct Part: Codable {
+                    let tag: Tag
+                    var text: String?
+                    var href: String?
+                    var userId: String?
+                    var userName: String?
+                    var imageKey: String?
+                }
+                
+                var title: String?
+                
+                var content: [[Part]]
+            }
+            
+            var zh_cn: Body?
+            
+            var en_us: Body?
+        }
+        
+        var post: Post? = nil
+        
+        var shareChatId: String?
+        
+        var imageKey: String?
     }
     
-    var content: Content = .init(text: "")
-    
+    var content: Content = .init(text: nil)
     
     enum CodingKeys: String, CodingKey {
         case type = "msg_type"
         case content
     }
+    
+    struct Card: Codable {
+        
+        struct Element: Codable {
+            
+            enum Tag: String, Codable {
+                case div
+            }
+            
+            let tag: Tag
+            
+            let text: String
+        }
+        
+        var elements: [Element]
+        
+        struct Header: Codable {
+            
+            struct Title: Codable {
+                
+                enum Tag: String, Codable {
+                    case plainText = "plain_text"
+                }
+                
+                let tag: Tag
+                
+                let content: String
+            }
+            
+            let title: Title
+        }
+        
+        var header: Header
+    }
+    
+    var card: Card?
 }
 
 extension Message {
@@ -42,13 +113,36 @@ extension Message {
     
     func text(_ text: String) -> Self {
         self.type = .text
-        self.content.text = text
+        self.content = .init(text: text)
         return self
     }
     
     func text(at userId: String) -> Self {
         self.type = .text
-        self.content.text = "<at user_id=\"\(userId)\"></at>"
+        self.content = .init(text: "<at user_id=\"\(userId)\"></at>")
+        return self
+    }
+    
+    func post(cnBody: Content.Post.Body? = nil, enBody: Content.Post.Body? = nil) -> Self {
+        self.type = .post
+        self.content = .init(post: .init(zh_cn: cnBody, en_us: enBody))
+        return self
+    }
+    
+    func shareChat(_ chatId: String) -> Self {
+        self.type = .shareChat
+        self.content = .init(shareChatId: chatId)
+        return self
+    }
+    
+    func image(_ imageKey: String) -> Self {
+        self.type = .image
+        self.content = .init(imageKey: imageKey)
+        return self
+    }
+    
+    func interactive() -> Self {
+        self.type = .interactive
         return self
     }
 }
@@ -56,9 +150,37 @@ extension Message {
 
 extension Message {
     
+    static let jsonEncoder: JSONEncoder = {
+        let jsonEncoder = JSONEncoder()
+        jsonEncoder.outputFormatting = [
+            .prettyPrinted,
+            .sortedKeys,
+        ]
+        jsonEncoder.keyEncodingStrategy = .convertToSnakeCase
+        return jsonEncoder
+    }()
+    
+    var formattedJson: String? {
+        
+        get throws {
+            
+            let data = try Self.jsonEncoder.encode(self)
+            
+            return String(data: data, encoding: .utf8)
+            
+        }
+    }
+    
     enum LarkError: Error {
         case noWebhook
         case requestFailed
+        case responseDataParseFailed
+        case responseBizError(msg: String)
+    }
+    
+    struct LarkResponse: Codable {
+        let code: Int
+        let msg: String
     }
     
     func sendToLark(with webhook: String) async throws -> Bool {
@@ -71,13 +193,22 @@ extension Message {
         var request = URLRequest(url: webhookURL)
         request.httpMethod = "POST"
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-        request.httpBody = try JSONEncoder().encode(self)
+        request.httpBody = try Self.jsonEncoder.encode(self)
         
-        let (_, response) = try await URLSession.shared.data(for: request)
+        let (data, response) = try await URLSession.shared.data(for: request)
         
         guard let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200
         else {
             throw LarkError.requestFailed
+        }
+        
+        guard let ret = try? JSONDecoder().decode(LarkResponse.self, from: data) else {
+            throw LarkError.responseDataParseFailed
+        }
+        
+        guard ret.code == 0
+        else {
+            throw LarkError.responseBizError(msg: ret.msg)
         }
         
         return true
--- Tests/OrzLarkSDKTests/OrzLarkSDKTests.swift
+++ Tests/OrzLarkSDKTests/OrzLarkSDKTests.swift
@@ -8,37 +8,17 @@ import XCTest
 // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
 final class OrzLarkSDKTests: XCTestCase {
     
-    let jsonEncoder: JSONEncoder = {
-        let jsonEncoder = JSONEncoder()
-        jsonEncoder.outputFormatting = [
-            .prettyPrinted,
-            .sortedKeys,
-        ]
-        return jsonEncoder
-    }()
-    
-    let webhook = "https://open.feishu.cn/open-apis/bot/v2/hook/1b4f84ed-6c61-4226-aa0b-81fea6804f8a"
+    static let userId = "ou_be45bb011eaaf304c1fa6a94242179b6"
     
     let textMessage = Message.make.text("string content")
     
     let textAtAllMessage = Message.make.text(at: "all")
     
-    let textAtUserMessage = Message.make.text(at: "d1535dda")
-    
-    func testSendTextMessage() async throws {
+    let textAtUserMessage = Message.make.text(at: userId)
         
-        let ret = try await textMessage.sendToLark(with: webhook)
-        
-        XCTAssertTrue(ret)
-    }
-    
     func testTextMessageEncodable() throws {
         
-        let data = try jsonEncoder.encode(textMessage)
-        
-        let jsonString = String(data: data, encoding: .utf8)!
-        
-        XCTAssertEqual(jsonString, """
+        XCTAssertEqual(try textMessage.formattedJson, """
         {
           "content" : {
             "text" : "string content"
@@ -48,17 +28,132 @@ final class OrzLarkSDKTests: XCTestCase {
         """)
     }
     
+    func testSendTextMessage() async throws {
+        
+        let ret = try await textMessage.sendToLark
+        
+        XCTAssertTrue(ret)
+    }
+    
+    
     func testSendTextAtAllMessage() async throws {
         
-        let ret = try await textAtAllMessage.sendToLark(with: webhook)
+        let ret = try await textAtAllMessage.sendToLark
         
         XCTAssertTrue(ret)
     }
     
     func testSendTextAtUserMessage() async throws {
         
-        let ret = try await textAtUserMessage.sendToLark(with: webhook)
+        let ret = try await textAtUserMessage.sendToLark
         
         XCTAssertTrue(ret)
+    }
+    
+    
+    let postMessage = Message.make.post(cnBody: .init(title: "test post", content: [
+        [
+            .init(tag: .text, text: "text post"),
+            .init(tag: .a, text: "百度链接", href: "https://www.baidu.com"),
+            .init(tag: .at, userId: userId)
+        ]
+    ]))
+    
+    func testPostMessageEncodable() throws {
+        
+        let ret = try postMessage.formattedJson
+        
+        XCTAssertEqual(ret, """
+        {
+          "content" : {
+            "post" : {
+              "zh_cn" : {
+                "content" : [
+                  [
+                    {
+                      "tag" : "text",
+                      "text" : "text post"
+                    },
+                    {
+                      "href" : "https:\\/\\/www.baidu.com",
+                      "tag" : "a",
+                      "text" : "百度链接"
+                    },
+                    {
+                      "tag" : "at",
+                      "user_id" : "ou_be45bb011eaaf304c1fa6a94242179b6"
+                    }
+                  ]
+                ],
+                "title" : "test post"
+              }
+            }
+          },
+          "msg_type" : "post"
+        }
+        """)
+    }
+    
+    func testSendPostMessage() async throws {
+      
+        let ret = try await postMessage.sendToLark
+        
+        XCTAssertTrue(ret)
+    }
+    
+    let shareChatMessage = Message.make.shareChat("oc_aa03b42c452ed330f6dafbdcd2fc92e7")
+    
+    func testShareChatMessageEncodable() throws {
+        
+        let ret = try shareChatMessage.formattedJson
+        
+        XCTAssertEqual(ret, """
+        {
+          "content" : {
+            "share_chat_id" : "oc_aa03b42c452ed330f6dafbdcd2fc92e7"
+          },
+          "msg_type" : "share_chat"
+        }
+        """)
+    }
+    
+    func testSendShareChatMessage() async throws {
+        
+        let ret = try await shareChatMessage.sendToLark
+        
+        XCTAssertTrue(ret)
+    }
+    
+    let imageMessage = Message.make.image("img_v3_025n_626413ac-9e2a-43e2-bba8-df231db0e93g")
+    
+    func testImageMessageEncodable() throws {
+        
+        let ret = try imageMessage.formattedJson
+        
+        XCTAssertEqual(ret, """
+        {
+          "content" : {
+            "image_key" : "img_v3_025n_626413ac-9e2a-43e2-bba8-df231db0e93g"
+          },
+          "msg_type" : "image"
+        }
+        """)
+    }
+    
+    func testSendImageMessage() async throws {
+        
+        let ret = try await imageMessage.sendToLark
+        
+        XCTAssertTrue(ret)
+    }
+}
+
+extension Message {
+    
+    static let testWebhook = "https://open.feishu.cn/open-apis/bot/v2/hook/993294fd-10b1-4385-8a37-053ac5f7bc67"
+    
+    var sendToLark: Bool {
+
+        get async throws { try await self.sendToLark(with: Self.testWebhook) }
     }
 }
